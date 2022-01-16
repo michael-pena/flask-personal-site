ddc240a85e687e2acdb815021e7b9ff99d0611db
Michael Pena <michael.pena@protonmail.com>
2022-01-16 17:47:40 -0600

    renamed all references to application

--- application.py
+++ application.py
@@ -3,6 +3,9 @@ from flask import render_template
 
 application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     return render_template("index.html")
+
+if __name__ == "__main__":
+       application.run()
